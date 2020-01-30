3186694094c3a7acb96a13fa51f6d8ce6a82b146
Andrew <cc6.magister@gmail.com>
2020-01-30 14:19:23 +0200

    update(text): add text to pixi layer

--- src/js/entities/buildings.ts
+++ src/js/entities/buildings.ts
@@ -1,17 +1,36 @@
 import * as PIXI from 'pixi.js'
 import { App, Visual } from '../interfaces'
 
+const labelStyle = new PIXI.TextStyle({
+	fontFamily: 'Arial',
+	fontSize: 14,
+})
+
+const names:Array<string> = [
+	'name1',
+	'name2',
+	'name3',
+	'name4',
+]
+
 export default class Building extends PIXI.Container {
 	sprite: PIXI.Sprite
 	phase: number
 	app: App
+	label: PIXI.Text
 	constructor(visualModel: Visual, app: App) {
 		super()
 
 		this.app = app
 		this.sprite = new PIXI.Sprite(visualModel.texture)
 		this.addChild(this.sprite)
-		this.phase = Math.random()
+		this.phase = 10*Math.random()
+
+		this.label = new PIXI.Text(names[Math.random()*names.length|0],labelStyle)
+		
+		// this.label.anchor.set(0.5, 1.0)
+		// this.label.zIndex = 1
+		this.addChild(this.label)
 
 		app.ticker.add((delta: number) => {
 			this.update(delta)
@@ -20,8 +39,11 @@ export default class Building extends PIXI.Container {
 
 	update(delta: number) {
 		this.phase += delta * 0.1
-		this.phase %= 2.0
-		const par:number = this.phase - 1
-		this.sprite.y = -50 * (1 - par * par)
+		this.phase %= 10.0
+		this.sprite.y = 0
+		if(this.phase<2.0){
+			const par:number = this.phase - 1
+			this.sprite.y = -50 * (1 - par * par)
+		}
 	}
 }
--- src/js/world.ts
+++ src/js/world.ts
@@ -15,13 +15,31 @@ export default class World extends PIXI.Container {
 	}
 
 	populate() {
-		for (let i = 0; i < 25; i++) {
-			const img = new Building(this.app.visual.buildings[i % 3], this.app)
-			
-			img.x = (i % 5 + 1) * img.width
-			img.y = Math.floor(i / 5 + 1) * img.height
-			
-			this.addChild(img)
+		for (let row = 0; row < 17; row++) {
+			for (let col = 0; col < 17; col++) {
+				if ((row + col) % 2 === 1) {
+					continue
+				}
+
+				const img = new Building(this.app.visual.buildings[row % 3], this.app)
+
+				img.x = (col + 1) * img.width / 2
+				img.y = (row + 1) * img.height / 2.4
+
+				this.addChild(img)
+			}
+
 		}
+
+
+
+		// for (let i = 0; i < 25; i++) {
+		// 	const img = new Building(this.app.visual.buildings[i % 3], this.app)
+
+		// 	img.x = (i % 5 + 1) * img.width
+		// 	img.y = Math.floor(i / 5 + 1) * img.height
+
+		// 	this.addChild(img)
+		// }
 	}
 }
